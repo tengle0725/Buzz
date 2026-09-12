12a8385788bd35ab283e7cd1d39ac9b0856c42b8
탱글 <tengle0725@gmail.com>
2026-09-12 10:56:53 +0900

    Initial Commit

---
+++ Buzz/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Buzz
+//
+//  Created by 탱글 on 9/12/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
